dce7586a9d2018805d9d64854cc31ec587c83e08
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-01 16:48:19 +0800

    add menu on core app for CLI. saveOrder implemented.

--- src/main/java/com/geybriyel/business/BusinessApplication.java
+++ src/main/java/com/geybriyel/business/BusinessApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class BusinessApplication {
@@ -18,9 +20,79 @@ public class BusinessApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(OrderDAO orderDAO) {
 		return runner -> {
-			saveOrder(orderDAO);
-			findOrderById(orderDAO);
+//			saveOrder(orderDAO);
+//			findOrderById(orderDAO);
+//			findOrderByLastName(orderDAO);
+//			findAllOrders(orderDAO);
+//			updateOrder(orderDAO);
+//			deleteOrder(orderDAO);
+//			deleteAllOrders(orderDAO);
+
+
+
+			System.out.println("Welcome to BusinessName");
+			System.out.println("========================");
+			Scanner scanner = new Scanner(System.in);
+			int transactionType;
+			do {
+				System.out.println("MENU");
+				System.out.println("1: Add an order");
+				System.out.println("2: Find an order by JO number");
+				System.out.println("3: Find order/s based on client's last name");
+				System.out.println("4: Delete an order by JO");
+				System.out.println("5: Delete all orders");
+				System.out.println("6: Quit");
+				System.out.print("Enter transaction: ");
+				transactionType = scanner.nextInt();
+			} while (transactionType < 1 || transactionType > 6);
+
+			System.out.println("You selected: " + transactionType);
+			switch (transactionType) {
+				case 1: {
+					saveOrder(orderDAO);
+					break;
+				}
+				case 2: {
+					findOrderById(orderDAO);
+				}
+			}
 		};
+	}
+
+	private void deleteAllOrders(OrderDAO orderDAO) {
+		System.out.println("Deleting all orders...");
+		int numDeleted = orderDAO.deleteAll();
+		System.out.println(numDeleted + " orders deleted");
+	}
+
+	private void deleteOrder(OrderDAO orderDAO) {
+		int id = 1;
+		orderDAO.delete(id);
+		System.out.println("Order deleted...");
+	}
+
+	private void updateOrder(OrderDAO orderDAO) {
+		int id = 1;
+		System.out.println("To update: order #" + id + ": " + orderDAO.findById(id));
+		RepairJO order = orderDAO.findById(id);
+		order.setLastName("...UPDATING...");
+		orderDAO.update(order);
+		System.out.println("Updated: " + orderDAO.findById(id));
+	}
+
+	private void printList(List<RepairJO> found) {
+		for (RepairJO instance : found) {
+			System.out.println(instance);
+		}
+	}
+	private void findAllOrders(OrderDAO orderDAO) {
+		System.out.println("Printing all orders...");
+		printList(orderDAO.findAll());
+	}
+
+	private void findOrderByLastName(OrderDAO orderDAO) {
+		System.out.println("...Found by last name....");
+		printList(orderDAO.findByLastName("Venema"));
 	}
 
 	private void findOrderById(OrderDAO orderDAO) {
@@ -29,14 +101,28 @@ public class BusinessApplication {
 	}
 
 	private void saveOrder(OrderDAO orderDAO) {
-		RepairJO tempOrder = new RepairJO();
-		tempOrder.setFirstName("Migo");
-		tempOrder.setLastName("Mogul");
-		tempOrder.setPhoneNumber("09090909909");
-		tempOrder.setTelephoneNumber("0000000");
-		tempOrder.setAddress("Migo Street");
+		System.out.println("\n...Create new job order...");
+		Scanner scanner = new Scanner(System.in);
 
-		orderDAO.save(tempOrder);
-		System.out.println("Order saved.");
+		System.out.print("Enter first name: ");
+		String firstName = scanner.nextLine();
+
+		System.out.print("Enter last name: ");
+		String lastName = scanner.nextLine();
+
+		System.out.print("Enter phone number (09xxxxxxxxx): ");
+		String phoneNumber = scanner.nextLine();
+
+		System.out.print("Enter telephone number (xxxxxxx): ");
+		String telephoneNumber = scanner.nextLine();
+
+		System.out.print("Enter address: ");
+		String address = scanner.nextLine();
+
+		RepairJO JO = new RepairJO(firstName, lastName, phoneNumber, telephoneNumber, address);
+		orderDAO.save(JO);
+
+		System.out.println("\nNew job order created with the following details: ");
+		System.out.println(JO);
 	}
 }
