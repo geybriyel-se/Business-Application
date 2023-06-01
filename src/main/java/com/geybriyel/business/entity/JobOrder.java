80521c795712a48085761a2c12706f308154975c
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-01 13:11:10 +0800

    map fields of entity class to database

--- src/main/java/com/geybriyel/business/entity/JobOrder.java
+++ src/main/java/com/geybriyel/business/entity/JobOrder.java
@@ -1,5 +1,34 @@
 package com.geybriyel.business.entity;
 
+import jakarta.persistence.*;
+
+import java.sql.Date;
+
+@Entity
+@Table(name = "job_order")
 public class JobOrder implements Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "job_order_number")
+    private int jobNumber;
+
+    @Column(name = "job_order_date")
+    private Date jobDate;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "telephone_number")
+    private String telephoneNumber;
+
+    @Column(name = "address")
+    private String address;
 
 }
