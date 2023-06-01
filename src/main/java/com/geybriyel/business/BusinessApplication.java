fe27257a12d0e71040c278acde8947948c80d9b2
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-01 13:41:47 +0800

    add save implementation to JO DAO

--- src/main/java/com/geybriyel/business/BusinessApplication.java
+++ src/main/java/com/geybriyel/business/BusinessApplication.java
@@ -1,7 +1,11 @@
 package com.geybriyel.business;
 
+import com.geybriyel.business.dao.OrderDAO;
+import com.geybriyel.business.entity.JobOrder;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BusinessApplication {
@@ -10,4 +14,22 @@ public class BusinessApplication {
 		SpringApplication.run(BusinessApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(OrderDAO orderDAO) {
+		return runner -> {
+			saveOrder(orderDAO);
+		};
+	}
+
+	private void saveOrder(OrderDAO orderDAO) {
+		JobOrder tempOrder = new JobOrder();
+		tempOrder.setFirstName("Migo");
+		tempOrder.setLastName("Mogul");
+		tempOrder.setPhoneNumber("09090909909");
+		tempOrder.setTelephoneNumber("0000000");
+		tempOrder.setAddress("Migo Street");
+
+		orderDAO.save(tempOrder);
+		System.out.println("Order saved.");
+	}
 }
