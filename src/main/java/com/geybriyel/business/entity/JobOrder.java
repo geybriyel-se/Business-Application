90a2988d26191086deb64a508959f9b249297832
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-01 13:13:24 +0800

    add constructor, getters and setters to entity class

--- src/main/java/com/geybriyel/business/entity/JobOrder.java
+++ src/main/java/com/geybriyel/business/entity/JobOrder.java
@@ -31,4 +31,79 @@ public class JobOrder implements Order {
     @Column(name = "address")
     private String address;
 
+    public JobOrder() {
+    }
+
+    public JobOrder(Date jobDate, String firstName, String lastName, String phoneNumber, String telephoneNumber, String address) {
+        this.jobDate = jobDate;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.telephoneNumber = telephoneNumber;
+        this.address = address;
+    }
+
+    public JobOrder(String firstName, String lastName, String phoneNumber, String telephoneNumber, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.telephoneNumber = telephoneNumber;
+        this.address = address;
+    }
+
+    public int getJobNumber() {
+        return jobNumber;
+    }
+
+    public void setJobNumber(int jobNumber) {
+        this.jobNumber = jobNumber;
+    }
+
+    public Date getJobDate() {
+        return jobDate;
+    }
+
+    public void setJobDate(Date jobDate) {
+        this.jobDate = jobDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
