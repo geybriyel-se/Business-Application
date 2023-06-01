b7c79d53d4f7daa0648c1cee7314f1a48574f801
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-01 15:13:05 +0800

    add findByLastName() implementation

--- src/main/java/com/geybriyel/business/dao/JobOrderDAOImpl.java
+++ src/main/java/com/geybriyel/business/dao/JobOrderDAOImpl.java
@@ -2,9 +2,12 @@ package com.geybriyel.business.dao;
 
 import com.geybriyel.business.entity.RepairJO;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class JobOrderDAOImpl implements OrderDAO {
@@ -25,5 +28,12 @@ public class JobOrderDAOImpl implements OrderDAO {
     @Override
     public RepairJO findById(Integer id) {
         return this.entityManager.find(RepairJO.class, id);
+    }
+
+    @Override
+    public List<RepairJO> findByLastName(String lastName) {
+        TypedQuery<RepairJO> query = entityManager.createQuery("FROM RepairJO WHERE lastName=:param", RepairJO.class);
+        query.setParameter("param", lastName);
+        return query.getResultList();
     }
 }
--- src/main/java/com/geybriyel/business/dao/OrderDAO.java
+++ src/main/java/com/geybriyel/business/dao/OrderDAO.java
@@ -2,8 +2,12 @@ package com.geybriyel.business.dao;
 
 import com.geybriyel.business.entity.RepairJO;
 
+import java.util.List;
+
 public interface OrderDAO {
     void save(RepairJO order);
 
     RepairJO findById(Integer id);
+
+    List<RepairJO> findByLastName(String lastName);
 }
