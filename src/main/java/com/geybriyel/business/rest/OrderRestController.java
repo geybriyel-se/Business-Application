83b1e0cf382131b46406c646366ef514ba708fe6
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-03 19:32:52 +0800

    add constructor to rest controller

--- src/main/java/com/geybriyel/business/rest/OrderRestController.java
+++ src/main/java/com/geybriyel/business/rest/OrderRestController.java
@@ -1,10 +1,17 @@
 package com.geybriyel.business.rest;
 
+import com.geybriyel.business.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class OrderRestController {
+    private OrderService orderService;
 
+    @Autowired
+    public OrderRestController(OrderService orderService) {
+        this.orderService = orderService;
+    }
 }
