3d346cc2799ce6c4721c23d0a9534f8170571192
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-03 20:08:46 +0800

    add get mapping for returning order by id

--- src/main/java/com/geybriyel/business/rest/OrderRestController.java
+++ src/main/java/com/geybriyel/business/rest/OrderRestController.java
@@ -4,6 +4,7 @@ import com.geybriyel.business.entity.RepairJO;
 import com.geybriyel.business.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,17 @@ public class OrderRestController {
     @GetMapping("/orders")
     public List<RepairJO> findAll() {
         return orderService.findAll();
+    }
+
+    // return an order by id
+    @GetMapping("/orders/{orderId}")
+    public RepairJO getOrderById(@PathVariable int orderId) {
+        RepairJO order = orderService.findById(orderId);
+
+        if (order == null) {
+            throw new RuntimeException("Order ID not found - " + orderId);
+        }
+
+        return order;
     }
 }
