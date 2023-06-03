faac5ee445c8118ae4e66f5413fce15fb047482c
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-03 20:47:51 +0800

    remove command line runner from core app

--- src/main/java/com/geybriyel/business/BusinessApplication.java
+++ src/main/java/com/geybriyel/business/BusinessApplication.java
@@ -1,12 +1,7 @@
 package com.geybriyel.business;
 
-import com.geybriyel.business.dao.OrderDAO;
-import com.geybriyel.business.entity.RepairJO;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class BusinessApplication {
@@ -15,28 +10,4 @@ public class BusinessApplication {
 		SpringApplication.run(BusinessApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(OrderDAO orderDAO) {
-		return runner -> {
-			saveOrder(orderDAO);
-			findOrderById(orderDAO);
-		};
-	}
-
-	private void findOrderById(OrderDAO orderDAO) {
-		System.out.println("Order Found...");
-		System.out.println(orderDAO.findById(1));
-	}
-
-	private void saveOrder(OrderDAO orderDAO) {
-		RepairJO tempOrder = new RepairJO();
-		tempOrder.setFirstName("Migo");
-		tempOrder.setLastName("Mogul");
-		tempOrder.setPhoneNumber("09090909909");
-		tempOrder.setTelephoneNumber("0000000");
-		tempOrder.setAddress("Migo Street");
-
-		orderDAO.save(tempOrder);
-		System.out.println("Order saved.");
-	}
 }
