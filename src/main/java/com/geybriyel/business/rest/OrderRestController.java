3feb4ab2d7bd37c227bd82c339af79760a77dd50
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-03 19:31:37 +0800

    add empty rest controller

---
+++ src/main/java/com/geybriyel/business/rest/OrderRestController.java
@@ -0,0 +1,10 @@
+package com.geybriyel.business.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class OrderRestController {
+
+}
