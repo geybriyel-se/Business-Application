4a736827878f456ae86f374c8fd5af1306cf5291
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-03 19:23:19 +0800

    remove transactional responsibility from the dao layer

--- src/main/java/com/geybriyel/business/dao/JobOrderDAOImpl.java
+++ src/main/java/com/geybriyel/business/dao/JobOrderDAOImpl.java
@@ -18,9 +18,7 @@ public class JobOrderDAOImpl implements OrderDAO {
         this.entityManager = entityManager;
     }
 
-
     @Override
-    @Transactional
     public void save(RepairJO order) {
         this.entityManager.persist(order);
     }
@@ -44,20 +42,12 @@ public class JobOrderDAOImpl implements OrderDAO {
     }
 
     @Override
-    @Transactional
-    public void update(RepairJO repairJO) {
-        this.entityManager.merge(repairJO);
-    }
-
-    @Override
-    @Transactional
     public void delete(Integer id) {
         RepairJO jo = findById(id);
         entityManager.remove(jo);
     }
 
     @Override
-    @Transactional
     public int deleteAll() {
         return entityManager.createQuery("DELETE FROM RepairJO").executeUpdate();
     }
