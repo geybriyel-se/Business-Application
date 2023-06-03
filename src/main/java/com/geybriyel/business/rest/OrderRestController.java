343218dd298a11abf2469cb083a2145bcc691d2b
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-03 19:59:02 +0800

    add get Cmapping for returning list of all orders

--- src/main/java/com/geybriyel/business/rest/OrderRestController.java
+++ src/main/java/com/geybriyel/business/rest/OrderRestController.java
@@ -1,9 +1,13 @@
 package com.geybriyel.business.rest;
 
+import com.geybriyel.business.entity.RepairJO;
 import com.geybriyel.business.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -13,5 +17,11 @@ public class OrderRestController {
     @Autowired
     public OrderRestController(OrderService orderService) {
         this.orderService = orderService;
+    }
+
+    // Return list of all orders
+    @GetMapping("/orders")
+    public List<RepairJO> findAll() {
+        return orderService.findAll();
     }
 }
