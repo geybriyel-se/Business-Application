d3eb0778892046847e594d914b9844f9e13967b7
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-05 18:35:27 +0800

    add authorization to GET all orders

--- src/main/java/com/geybriyel/business/security/SecurityConfig.java
+++ src/main/java/com/geybriyel/business/security/SecurityConfig.java
@@ -1,8 +1,13 @@
 package com.geybriyel.business.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
@@ -10,9 +15,23 @@ import javax.sql.DataSource;
 public class SecurityConfig {
 
     // add jdbc support for default spring schema
+    @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource) {
         return new JdbcUserDetailsManager(dataSource);
     }
 
+    // add authorization for each endpoint
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/orders").hasRole("EMPLOYEE")
+                );
 
+
+
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.csrf(csrf -> csrf.disable());
+        return httpSecurity.build();
+    }
 }
