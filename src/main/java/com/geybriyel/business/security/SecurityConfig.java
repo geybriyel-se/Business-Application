571a663fe764a0cf9eaf3b9c3f1701a522ee80d5
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-05 18:25:11 +0800

    add jdbc support for user detail manager using default schema

--- src/main/java/com/geybriyel/business/security/SecurityConfig.java
+++ src/main/java/com/geybriyel/business/security/SecurityConfig.java
@@ -1,8 +1,18 @@
 package com.geybriyel.business.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
+
+    // add jdbc support for default spring schema
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
 
 }
