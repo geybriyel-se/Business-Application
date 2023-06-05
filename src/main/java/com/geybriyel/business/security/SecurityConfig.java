fb16cec22ec2ce98f43a828b2c13e29c996369c6
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-05 18:56:11 +0800

    add authorization to PUT (update) an order

--- src/main/java/com/geybriyel/business/security/SecurityConfig.java
+++ src/main/java/com/geybriyel/business/security/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/orders/id/**").hasRole("EMPLOYEE")
                         .requestMatchers(HttpMethod.GET, "/api/orders/lastname/**").hasRole("EMPLOYEE")
                         .requestMatchers(HttpMethod.POST, "/api/orders").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/orders").hasRole("MANAGER")
                 );
 
 
