9eff976ab8a8538eee7aa90652cc9b2f1b66992a
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-07 15:32:08 +0800

    add custom methods to spring data jpa interface repo

--- src/main/java/com/geybriyel/business/dao/OrderRepository.java
+++ src/main/java/com/geybriyel/business/dao/OrderRepository.java
@@ -3,6 +3,9 @@ package com.geybriyel.business.dao;
 import com.geybriyel.business.entity.RepairJO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<RepairJO, Integer> {
+import java.util.List;
 
+public interface OrderRepository extends JpaRepository<RepairJO, Integer> {
+    public List<RepairJO> findByLastName(String lastName);
+    public void deleteAll();
 }
